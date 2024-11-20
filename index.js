57a1a5f0ea9dff824cd2e5f98d12285b8a4b4da9
hadi12100 <barkallahhadi579@gmail.com>
2024-11-20 11:37:15 +0100

    first commit

--- index.js
+++ index.js
@@ -58,3 +58,65 @@ function fibonacci(n) {
     }
     return fib.slice(0, n);
 }
+//
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+function countCharacters(str) {
+    return str.length;
+}
+function capitalizeWords(sentence) {
+    return sentence
+        .split(' ') 
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1)) 
+        .join(' ');
+}
+///
+function factorial(n) {
+    if (n < 0) {
+        return "Factorial is not defined for negative numbers.";
+    }
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+console.log(factorial(5));
+console.log(factorial(8));
+
+
+function isPrime(num) {
+    if (num <= 1) {
+        return false; 
+    }
+    if (num === 2) {
+        return true; 
+    }
+    if (num % 2 === 0) {
+        return false; 
+    }
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) {
+            return false; 
+        }
+    }
+    return true;
+}
+console.log(isPrime(12));
+console.log(isPrime(17));
+
+
+function genererFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+
+    const suite = [0, 1];
+    for (let i = 2; i < n; i++) {
+        suite.push(suite[i - 1] + suite[i - 2]);
+    }
+    return suite;
+}
+console.log(genererFibonacci(10)); 
+console.log(genererFibonacci(14));
